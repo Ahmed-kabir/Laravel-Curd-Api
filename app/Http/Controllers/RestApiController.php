6cac528c2deea3f2265efb947f9eb3d25e8382c3
kabir <kabir14235@gmail.com>
2020-03-31 15:26:23 +0600

    Api Read Data

---
+++ app/Http/Controllers/RestApiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RestApi;
+use Illuminate\Http\Request;
+
+class RestApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RestApi  $restApi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RestApi $restApi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RestApi  $restApi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RestApi $restApi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RestApi  $restApi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RestApi $restApi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RestApi  $restApi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RestApi $restApi)
+    {
+        //
+    }
+}
